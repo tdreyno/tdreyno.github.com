6189030c5ef6bb2a42e8fb08b92d62136d6083f4
Thomas Reynolds <me@tdreyno.com>
2011-08-23 18:16:11 -0700

    update article

--- javascripts/jqwidont.js
+++ javascripts/jqwidont.js
@@ -20,4 +20,4 @@
  * implementation, visit:
  *   * http://davecardwell.co.uk/javascript/jquery/plugins/jquery-widont/
  */
-new function(){function d(a){return a.replace(b.regexp,"&#160;$1")}function c(){$("h1,h2,h3,h4,h5,h6").widont()}var a={auto:function(a){return a!=undefined?b.auto=a:b.auto},init:function(){return b.init()},transform:function(a){return b.widont(a)}};$.jqwidont=a;var b={auto:!0,init:c,widont:d,regexp:new RegExp("[\\n\\r\\s]+([^\\n\\r\\s(?:&#160;)]+[\\n\\r\\s]*)$","m")};$(document).ready(function(){b.auto&&c()}),$.fn.widont=function(){return $(this).each(function(){var a=$(this);a.html(b.widont(a.html()))})}}
+new function(){function c(){$("h1,h2,h3,h4,h5,h6").widont()}function d(a){return a.replace(b.regexp,"&#160;$1")}var a={auto:function(a){return a!=undefined?b.auto=a:b.auto},init:function(){return b.init()},transform:function(a){return b.widont(a)}};$.jqwidont=a;var b={auto:!0,init:c,widont:d,regexp:new RegExp("[\\n\\r\\s]+([^\\n\\r\\s(?:&#160;)]+[\\n\\r\\s]*)$","m")};$(document).ready(function(){b.auto&&c()}),$.fn.widont=function(){return $(this).each(function(){var a=$(this);a.html(b.widont(a.html()))})}}
